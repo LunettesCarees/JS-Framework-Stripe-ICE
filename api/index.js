73207f18b0a20e36a0a4a815d1ee8a822ca35cc4
Ryan Chen <113140112+LunettesCarees@users.noreply.github.com>
2024-08-08 16:58:24 -0400

    Update index.js to use bodyParser

--- api/index.js
+++ api/index.js
@@ -1,52 +1,29 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const routes = require('./routes/app.routes');
+
+var cors = require('cors');
+
+const bodyParser = require('body-parser');
 
 const corsOpt = {
-    origin: 'https://js-framework-tutorial-stripe.onrender.com',
-    methods: ['POST'],
-    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Origin'],
+    origin: '*',
+
+    methods: [
+        'post'
+    ],
+
+    allowedHeaders: [
+        'Access-Control-Allow-Origin',
+        'Origin'
+    ]
 };
 
 app.use(cors(corsOpt));
-app.use(express.json());
-app.use("/api", routes);
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-});
+app.use(bodyParser.json());
 
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
+app.use('/api', require('./routes/app.routes'))
 
-// // Replace with your frontend URL on Render
-// const frontendURL = 'https://your-frontend-service.onrender.com';
-
-// const corsOptions = {
-//     origin: frontendURL,  // Allow only your frontend domain
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Specify allowed methods
-//     allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Origin', 'Authorization'],  // Add any additional headers your application uses
-// };
-
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use("/api", require("./routes/app.routes"));
-
-// app.listen(4000, () => {
-//     console.log("Server is running on port 4000");
-// });
-
-const STRIPE_CONFIG = {
-    SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    CURRENCY: process.env.STRIPE_CURRENCY,
-    SUCCESS_URL: process.env.STRIPE_SUCCESS_URL,
-    CANCEL_URL: process.env.STRIPE_CANCEL_URL
-};
-
-module.exports = {
-    STRIPE_CONFIG
-};
+app.listen(4000, function () {
+    console.log('Server is running on port 4000');
+})
